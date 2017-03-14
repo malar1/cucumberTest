2e0912fcb5d509043bb01dc5056d2de4822e1326
kavi malar <kavi malar>
2017-03-14 09:06:49 +0530

    first commit

---
+++ src/test/java/googleTest/GoogleTest.java
@@ -0,0 +1,14 @@
+package googleTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	@Test
+	public void sampletest() {
+		WebDriver wd = new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.co.in");
+	}
+}
